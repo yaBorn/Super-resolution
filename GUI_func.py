2af1826ab3183eddacdb306bf1b9528cf74082b5
yaBorn <yaborn@foxmail.com>
2021-05-25 00:36:38 +0800

    gui 文件选择框 获取输入输出路径

--- GUI_func.py
+++ GUI_func.py
@@ -2,22 +2,31 @@
     图形界面 回调函数
 """
 import attribute as at
+import Base_func as bf
 import tkinter as tk  # 使用Tkinter_GUI包
 from tkinter import filedialog, END
 
 
 # 选择文件按钮
-def func_chooseFile(entry):
+def func_chooseFile(entryInput, entryOutput):
     # 打开文件选择对话框 返回选择文件路径
     filename = tk.filedialog.askopenfilename()
     if filename:
-        print("选择文件:", filename)
+        # 分割字符串 处理为
         at.file_input = filename
+        at.file_output = bf.getOutputFile(filename)
+        print("输入文件:", at.file_input)
+        print("预输出文件:", at.file_output)
         # 设置文本框内容
-        entry.config(state='normal')  # 写入后设为只读
-        entry.delete(0, END)  # 删除entry框内容
-        entry.insert(0, filename)  # entry显示当前路径
-        entry.config(state='readonly')  # 写入后设为只读
+        entryInput.config(state='normal')  # 写入后设为只读
+        entryInput.delete(0, END)  # 删除entry框内容
+        entryInput.insert(0, at.file_input)  # entry显示当前路径
+        entryInput.config(state='readonly')  # 写入后设为只读
+
+        entryOutput.config(state='normal')
+        entryOutput.delete(0, END)
+        entryOutput.insert(0, at.file_output)
+        entryOutput.config(state='readonly')
     else:
         print("未找到该文件")
 
--- GUI_main.py
+++ GUI_main.py
@@ -15,20 +15,30 @@ title.pack()
 ''' 输入路径 '''
 title_input = tk.Label(window, text='输入路径：', font=('Arial', 11), width=30, height=2)
 title_input.pack()
-#title_input.place(relx=0.1, rely=0.25, relwidth=0.8)
-
+# title_input.place(relx=0.1, rely=0.25, relwidth=0.8)
 text_input = tk.Entry(window, show=None, state='normal')  # 无密文 可写
 text_input.insert(0, "未选择...")  # 写入文本
 text_input.config(state='readonly')  # 写入后设为只读
 text_input.pack()
-#text_input.place(relx=0.1, rely=0.25, relwidth=0.8)
+text_input.place(relx=0.1, rely=0.25, relwidth=0.8)
+
+''' 输出路径'''
+title_output = tk.Label(window, text='输出路径：', font=('Arial', 11), width=30, height=2)
+title_output.pack()
+text_output = tk.Entry(window, show=None, state='normal')  # 无密文 可写
+text_output.insert(0, "未选择...")  # 写入文本
+text_output.config(state='readonly')  # 写入后设为只读
+text_output.pack()
+text_output.place(relx=0.1, rely=0.4, relwidth=0.8)
 
 
 ''' 选择文件按钮'''
 button_chooseFile = tk.Button(window, text='选择文件', font=('Arial', 11), width=10, height=1,
-                              command=lambda: func_chooseFile(entry=text_input))
+                              command=lambda: func_chooseFile(entryInput=text_input, entryOutput=text_output))
 # command是按钮回调函数 lambda: 传递参数
 button_chooseFile.pack()
+
+
 
 # 主窗口循环显示
 window.mainloop()
